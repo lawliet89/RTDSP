e8e56a05a647d6058514d927cad718ebe4a8c7cc
Yong Wen Chua <yongwen.chua10@imperial.ac.uk>
2013-03-17 09:35:07 +0000

    Matlab file to plot Spectrogram

---
+++ project/resources/wav2spec.m
@@ -0,0 +1,20 @@
+function [S,F,T,P] = wav2spec( filename )
+    % takes in a file name, calculates and/or plots spectrogram, and returns it
+    
+    % parameters
+    fftlength = 256;
+    overlap = 4;
+    
+    [wav, fs] = audioread(filename);
+    
+    % calculates spectrogram
+    [S,F,T,P] = spectrogram(wav(:,1), fftlength, overlap, fftlength, fs);
+    
+    % plot
+    figure;
+    spectrogram(wav(:,1), fftlength, overlap, fftlength, fs, 'yaxis');
+    
+    % comment the next line to not rotate
+    view([-47 22]);
+end
+
