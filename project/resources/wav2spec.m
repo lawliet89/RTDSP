0dd48e6a8c24807369ce9ce3005c459d6fa79566
Yong Wen Chua <yongwen.chua10@imperial.ac.uk>
2013-03-17 15:22:36 +0000

    Wav2spec now uses Mike Brooke's Voicebox to plot prettier spectrograms

--- project/resources/wav2spec.m
+++ project/resources/wav2spec.m
@@ -1,26 +1,13 @@
-function [S,F,T,P] = wav2spec( filename )
+function wav2spec( filename )
     % takes in a file name, calculates and/or plots spectrogram, and returns it
     
-    % parameters
-    fftlength = 256;
-    overlap = 4;
     
     [wav, fs] = audioread(filename);
-    
-    % calculates spectrogram
-    [S,F,T,P] = spectrogram(wav(:,1), fftlength, overlap, fftlength, fs);
-    
+
     % plot
     figure;
-    spectrogram(wav(:,1), fftlength, overlap, fftlength, fs, 'yaxis');
+    addpath('voicebox');
+    spgrambw(wav, fs, 'Jclw');
     
-    % comment the next line to not rotate
-    %view([-47 22]);
-    
-    % show the colour bar
-    colorbar('EastOutside');
-    
-    % limit colorbar range
-    caxis([-160, -20]);
 end
 
